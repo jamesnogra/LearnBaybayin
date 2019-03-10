70bc7f0b3ff1a8b2d258f6f3f5f507a9088afaa0
jamesnogra <love.gerald@yahoo.com>
2019-03-10 11:05:47 +0800

    Changes to the tutorial image.

--- platforms/android/app/build/intermediates/assets/release/www/js/signin.js
+++ platforms/android/app/build/intermediates/assets/release/www/js/signin.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 		var password = $('#password').val();
 		if (email.length>0 && password.length>0) {
 			addLoader();
-			$.post("http://learnbaybayinbackend.iamcebu.com/index.php/user/login", {email:email, password:password}, function(result) {
+			$.post("http://learnbaybayinbackend.iamcebu.com/index.php/user/login", {email:email, password:password}).done(function(result) {
 				if (result.status == 1) {
 					localStorage.setItem("login_token", result.user.login_token);
 					localStorage.setItem("name", result.user.name);
@@ -16,6 +16,9 @@ $(document).ready(function() {
 					removeLoader();
 					customAlert("Login Error", "The email or password you entered does not match.", "OK");
 				}
+			}).fail(function() {
+				removeLoader();
+				customAlert("Server Did Not Respond", "The server did not respond. Please report to nogra@outlook.com.", "OK");
 			});
 		}
 	});
