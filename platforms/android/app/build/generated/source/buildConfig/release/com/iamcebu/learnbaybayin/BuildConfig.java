867ee22404064f5208114cd2e63a95a50f612bb3
James Arnold <james.arnold.nogra@fullspeedtechnologies.com>
2020-07-28 14:53:55 +0800

    After publish in changing internet connection settings.

--- platforms/android/app/build/generated/source/buildConfig/release/com/iamcebu/learnbaybayin/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/release/com/iamcebu/learnbaybayin/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.iamcebu.learnbaybayin";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 20002;
-  public static final String VERSION_NAME = "2.0.2";
+  public static final int VERSION_CODE = 20003;
+  public static final String VERSION_NAME = "2.0.3";
 }
